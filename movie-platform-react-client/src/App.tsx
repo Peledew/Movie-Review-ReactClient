b73a4a3b15f89e5a65e365eb61e19b94f0ace580
Luka-0 <93083090+Peledew@users.noreply.github.com>
2024-12-11 19:35:51 +0100

    Creating new project

---
+++ movie-platform-react-client/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const App: React.FC = () => {
+  return (
+    <div className="container">
+      <h1 className="center-align">Hello, Materialize!</h1>
+      <p className="flow-text">This is a simple React app using Vite, TypeScript, and MaterializeCSS.</p>
+      <button className="btn waves-effect waves-light">Click Me</button>
+    </div>
+  );
+};
+
+export default App;
