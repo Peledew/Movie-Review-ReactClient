16727edff456de22f7b2589cdf356590097b8e48
Luka-0 <93083090+Peledew@users.noreply.github.com>
2024-12-12 14:49:38 +0100

    Prettier setup

--- movie-platform-react-client/src/main.tsx
+++ movie-platform-react-client/src/main.tsx
@@ -1,11 +1,11 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
 import 'materialize-css/dist/css/materialize.min.css';
-import App from './App.tsx'
+import App from './App.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
   </StrictMode>,
-)
+);
